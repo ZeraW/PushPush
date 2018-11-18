3aa7bb85f2745961354771a3949c05dac1c4cb38
ZeraW <hema_eie552@yahoo.com>
2018-11-18 12:59:22 +0200

    added the save class

---
+++ app/src/main/java/gmsproduction/com/pushpush/Models/SaveLinks.java
@@ -0,0 +1,9 @@
+package gmsproduction.com.pushpush.Models;
+
+/**
+ * Created by Hima on 11/18/2018.
+ */
+
+public class SaveLinks {
+    String url_for_Room = "https://codelabs.developers.google.com/codelabs/android-room-with-a-view/#6";
+}
